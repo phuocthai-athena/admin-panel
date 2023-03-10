97b1e7ea8babb4672d875f9a98b1b5373cf10ec9
ThaiBuiEverfit <thaibui@everfit.io>
2023-03-10 20:14:16 +0700

    first commit

---
+++ src/PrivateRouter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const PrivateRouter = ({ component: Component, ...rest }) => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  return (
+    <Route
+      {...rest}
+      component={(props) => {
+        if (
+          true
+          // userInfo && userInfo.isAdmin
+        ) {
+          return <Component {...props} />;
+        } else {
+          return <Redirect to={"/login"} />;
+        }
+      }}
+    />
+  );
+};
+
+export default PrivateRouter;
